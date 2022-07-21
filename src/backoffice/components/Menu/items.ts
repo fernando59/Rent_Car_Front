1e162b0b93be4a463e771232e8b7f47c2c3f1e05
fernando <fernandomercado529@gmail.com>
2022-07-21 11:36:09 -0400

    Change icons items

--- src/backoffice/components/Menu/items.ts
+++ src/backoffice/components/Menu/items.ts
@@ -3,10 +3,11 @@ export const menuItems = [
         label: 'Home',
         items: [
             { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/backoffice' } ,
-            { label: 'Vehicles', icon: 'pi pi-fw pi-home', to: '/backoffice', items:[
-                { label: 'Brands', icon: 'pi pi-fw pi-home', to: '/backoffice' } ,
-                { label: 'Models', icon: 'pi pi-fw pi-home', to: '/backoffice' } ,
+            { label: 'Vehicles', icon: 'pi pi-fw pi-car', to: '/backoffice', items:[
+                { label: 'Brands', icon: 'pi pi-fw pi-bookmark', to: '/backoffice' } ,
+                { label: 'Models', icon: 'pi pi-fw pi-book', to: '/backoffice' } ,
             ]} ,
+            { label: 'Orders', icon: 'pi pi-fw pi-wallet', to: '/backoffice' } ,
         ],
         
     },
